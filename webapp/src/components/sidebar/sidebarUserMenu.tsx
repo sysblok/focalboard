f7f7205c988b4becaca6d3f9932f4ee7def0542b
Nastasia <lisitsinan@gmail.com>
2025-08-25 21:11:32 +0300

    feat: set dev or prod by env in Docker

--- webapp/src/components/sidebar/sidebarUserMenu.tsx
+++ webapp/src/components/sidebar/sidebarUserMenu.tsx
@@ -14,7 +14,6 @@ import MenuWrapper from '../../widgets/menuWrapper'
 import {getMe, isAdmin, setMe} from '../../store/users'
 import {useAppSelector, useAppDispatch} from '../../store/hooks'
 import {Utils} from '../../utils'
-import {getClientConfig} from '../../store/clientConfig'
 
 import ModalWrapper from '../modalWrapper'
 
@@ -33,7 +32,6 @@ const SidebarUserMenu = () => {
     const user = useAppSelector<IUser|null>(getMe)
     const isUserAdmin = useAppSelector<boolean>(isAdmin);
     const intl = useIntl()
-    const clientConfig = useAppSelector(getClientConfig)
 
     if (Utils.isFocalboardPlugin()) {
         return <></>
@@ -45,7 +43,7 @@ const SidebarUserMenu = () => {
                     <div className='logo'>
                         <div className='logo-title'>
                             <FocalboardLogoIcon/>
-                            <span>{clientConfig.featureFlags['FOCALBOARD_ENVIRONMENT'] == 'prod' ? 'Борда' : 'Focalboard'}</span>
+                            <span>{process.env.FOCALBOARD_ENVIRONMENT == 'prod' ? 'Борда' : 'Focalboard'}</span>
                             <div className='versionFrame'>
                                 <div
                                     className='version'
--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -13,7 +13,6 @@ import {UserSettings} from './userSettings'
 import {IUser} from './user'
 import {getMe} from './store/users'
 import {useAppSelector} from './store/hooks'
-import {getClientConfig} from './store/clientConfig'
 
 import '@mattermost/compass-icons/css/compass-icons.css'
 
@@ -32,17 +31,14 @@ initThemes()
 
 const MainApp = () => {
     const me = useAppSelector<IUser|null>(getMe)
-    const clientConfig = useAppSelector(getClientConfig)
 
-    useEffect(() => {
-        if (clientConfig.featureFlags['FOCALBOARD_ENVIRONMENT'] != 'prod') {
-            // TODO also set this when changing a theme
-            // for some reason useAppSelector doesn't work in theme.ts
-            document.documentElement.style.setProperty('--sidebar-bg-rgb', '92, 50, 30')
-        } else {
-            document.documentElement.style.setProperty('--sidebar-bg-rgb', '30, 50, 92')
-        }
-    }, [clientConfig.featureFlags['FOCALBOARD_ENVIRONMENT']])
+    const environment = process.env.FOCALBOARD_ENVIRONMENT
+
+    if (environment !== 'prod') {
+        document.documentElement.style.setProperty('--sidebar-bg-rgb', '92, 50, 30')
+    } else {
+        document.documentElement.style.setProperty('--sidebar-bg-rgb', '30, 50, 92')
+    }
 
 
     return (
